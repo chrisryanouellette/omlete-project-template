4d3f1ded2e5f89a8d27550d1edf4430dfab14cb9
Chris <couellette@webstaurantstore.com>
2022-07-09 20:48:47 -0400

    feat - Finished main script

--- bin/index.ts
+++ bin/index.ts
@@ -2,8 +2,12 @@
 
 import { resolve } from "path";
 import { ask, askOptions, askYesOrNo } from "./utilities/ask";
+import { hasKey } from "./utilities";
 import { mkdir } from "./utilities/file/dir";
-import constants from "./config.json";
+import { deepReadWrite, readWrite } from "./utilities/file";
+import { findAndReplace } from "./utilities/file/replace";
+import { exec } from "./utilities/exec";
+import config from "./config.json";
 import "./prerun";
 
 const main = async (): Promise<void> => {
@@ -14,19 +18,23 @@ const main = async (): Promise<void> => {
   if (env.interactive === "true") {
     env.template = await askOptions(
       "What kind of template would you like to use?",
-      constants.cliOpts.template.options
+      config.cliOpts.template.options
     );
 
     env.project = await ask(
-      "What is the name of your project? Default: my-project"
+      "What is the name of your project? Default: project-template"
     );
 
     if (!env.project) {
-      env.project = constants.cliOpts.project.default;
+      env.project = config.cliOpts.project.default;
     }
 
     env.install = (
       await askYesOrNo("Would you like to install the dependencies?")
+    ).toString();
+
+    env.git = (
+      await askYesOrNo("Would you like to initial a git repo?")
     ).toString();
   }
 
@@ -38,9 +46,47 @@ const main = async (): Promise<void> => {
 
   // Begin writing files
 
-  const dir = resolve(process.cwd(), env.project);
+  const src = resolve(__dirname, "../template");
+  const dest = resolve(process.cwd(), env.project);
+  const templates = hasKey(env.template, config.templateAlias)
+    ? config.templateAlias[env.template]
+    : [env.template];
+  const fsPromises: Promise<void>[] = [];
 
-  await mkdir(dir);
+  await mkdir(dest);
+
+  fsPromises.push(readWrite(src, dest));
+
+  /** Write template files */
+  for (const template of templates) {
+    fsPromises.push(
+      (async (): Promise<void> => {
+        const templateSrcDir = resolve(src, template);
+        const templateDestDir = resolve(dest, template);
+
+        await mkdir(templateDestDir);
+        await deepReadWrite(templateSrcDir, templateDestDir);
+      })()
+    );
+  }
+
+  await Promise.all(fsPromises);
+
+  /** Rename files and rewrite file contents */
+
+  await findAndReplace(dest);
+
+  /** Install dependencies */
+  if (process.env.install === "true") {
+    await exec("yarn", dest);
+  }
+
+  /** Create git repo and commit */
+  if (process.env.git === "true") {
+    await exec("git init", dest);
+    await exec("git add *", dest);
+    await exec('git commit -m "Initial Commit"', dest);
+  }
 };
 
 // The main process
